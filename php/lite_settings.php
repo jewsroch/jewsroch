a434c4bb8060129ce03091e8e05b21c1dce74236
Chad Jewsbury <chadjewsbury@gmail.com.com>
2013-04-28 12:18:31 -0600

    Adds files to repo

---
+++ php/lite_settings.php
@@ -0,0 +1,8 @@
+<?php
+
+
+$email_to = "spicyfishy@gmail.com"; // your email address
+$email_subject = "Hello from Jewsroch.com"; // email subject line
+$thankyou = "../thankyou.html"; // thank you page
+
+?>
